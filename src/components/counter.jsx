283612a62e4bd63ace078c23109f726c39c8054f
Ndumiso Mguni <Ndumiso29@gmail.com>
2019-07-09 13:35:46 -0400

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { brotliDecompress } from "zlib";
+
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  handleIncrement = product => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement(product)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "zero" : count;
+  }
+}
+
+export default Counter;
